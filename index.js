de2f9c8f1217d8c88f652d328162b881485cba42
ArjunPraveen <arjun.praveen@learner.manipal.edu>
2021-01-14 03:28:59 +0400

    start script

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const path = require('path')
+import cors from 'cors';
 app.use(express.json({ extended: false }));
+app.use(cors())
 
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
@@ -59,7 +61,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req,res)=> {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    })  
 }
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
 
